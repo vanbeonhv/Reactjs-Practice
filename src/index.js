7c59acb21d0b1611565719465d3652317b4fdfe5
Phantom Lancer <nguyenvannhv26@gmail.com>
2022-09-17 11:15:17 +0700

    Revert "add login route"
    This reverts commit 5ab2e5fb764d760d161c2fa35d0fc197bd69f4e2.

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import "./index.css";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import LandingPage from "./components/DemoLopHoc/Router/LoginPage/LandingPage";
+import LoginPage from "./components/DemoLopHoc/Router/LoginPage/LoginPage";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<LandingPage />);
+root.render(<LoginPage />);
